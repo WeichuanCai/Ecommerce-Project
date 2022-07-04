17a26d8c5f0113eb5e5b51813c464e99c9f2f7da
WeichuanCai <73470479+WeichuanCai@users.noreply.github.com>
2022-07-04 11:05:31 -0400

    update the pagination and delete the old version

---
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import {ProductService} from 'src/app/services/product.service';
+import {ActivatedRoute} from '@angular/router';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-grid.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
+  searchMode: boolean = false;
+
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
+  
+
+  constructor(private ProductService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
+    
+  }
+  listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if(this.searchMode) {
+      this.handleSearchProducts();
+    }else{
+      this.handleListProducts();
+    }
+
+    
+ 
+  }
+  handleSearchProducts() {
+    const theKeyword:string = String(this.route.snapshot.paramMap.get('keyword'));
+    this.ProductService.searchProducts(theKeyword).subscribe(
+      data => {this.products = data;}
+    )
+  }
+ 
+
+  handleListProducts(){
+
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if(hasCategoryId) {
+      this.currentCategoryId! = + Number(this.route.snapshot.paramMap.get('id'));
+    }else{
+      this.currentCategoryId = 1;
+    }
+
+    if(this.previousCategoryId != this.currentCategoryId){
+      this.thePageNumber = 1;
+    }
+    this.previousCategoryId = this.currentCategoryId;
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+    this.ProductService.getProductListPaginate(this.thePageNumber-1, this.thePageSize, this.currentCategoryId)
+    .subscribe(this.processResult());
+  }
+  processResult(){
+    return (data:any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    };
+  }
+}
