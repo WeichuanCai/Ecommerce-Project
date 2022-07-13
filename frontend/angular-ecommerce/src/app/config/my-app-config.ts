6eb6d2316ebf92ca340cb20434ec6535e6e727f2
WeichuanCai <73470479+WeichuanCai@users.noreply.github.com>
2022-07-13 19:40:28 -0400

    add the login/logout security with Okta

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId:'0oa5rog6zxIxyjjMD5d7',
+        issuer: 'https://dev-09199817.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
