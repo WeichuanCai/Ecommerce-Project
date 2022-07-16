a549b7ed2f90a3180d31a89f559dec599de7ac91
WeichuanCai <73470479+WeichuanCai@users.noreply.github.com>
2022-07-15 20:34:24 -0400

    check the unique email

--- 02-backend/spring-boot-ecommerce/src/main/java/com/weichuan/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/weichuan/ecommerce/config/MyDataRestConfig.java
@@ -29,7 +29,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 //        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         //disable HTTP methods: PUT, POST and DELETE
         disableHttpMethods(Product.class, config, theUnsupportedActions);
